f6c3878579624b2578204338fed7d72464fc0b4a
JunayetTamim <junayettamim186@gmail.com>
2022-04-04 01:54:09 +0600

    Dynamic Route set

--- src/components/HomeReview/HomeReview.js
+++ src/components/HomeReview/HomeReview.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useReviews from '../Hooks/useReviews';
 
 const HomeReview = () => {
+    const [reviews, setReviews] = useReviews();
+    const navigate = useNavigate();
+
+    const handleSeeAllReview =()=>{
+        const path = '/reviews'
+        navigate(path);
+    }
     return (
         <div>
             <h1 className='text-5xl mt-8'>Customer Reviews</h1>
-            <button>See All Review</button>
+            
+            {
+                reviews.slice(0,3).map(review =>
+                <h2>Name:{review.name}</h2>
+                )
+            }
+            <button onClick={handleSeeAllReview}>See All Review : {reviews.length}</button>
         </div>
     );
 };
