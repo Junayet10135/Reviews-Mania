89b79ddf7ce15a1b0de0517d9a3e5e7c625d6216
JunayetTamim <junayettamim186@gmail.com>
2022-04-04 00:30:24 +0600

    Home add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Laptop from "../../Assets/Image/Laptop.png";
+import HomeReview from '../HomeReview/HomeReview';
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <div className='Home-container'>
+            <div className='home-info-container'>
+                <h1 className='text-5xl font-bold text-blue-400 mb-4'>We Are Here For You</h1>
+                <h1 className='text-4xl font-bold text-purple-300'>Know Whats Best For you</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius consectetur vel atque mollitia inventore excepturi culpa libero adipisci saepe. Id.</p>
+                
+            </div>
+            <div className='home-image-container'>
+                <img src={Laptop} alt="" />
+            </div>
+            <button>More Info</button>
+            
         </div>
+            <HomeReview></HomeReview>
+        </div>
+        
     );
 };
 
---
+++ src/components/HomeReview/HomeReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeReview = () => {
+    return (
+        <div>
+            <h1 className='text-5xl mt-8'>Customer Reviews</h1>
+            <button>See All Review</button>
+        </div>
+    );
+};
+
+export default HomeReview;
